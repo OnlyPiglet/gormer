d32df98957ca31e115d705166d8afd71076e6ea5
OnlyPiglet <jackwuchenghao4@gmail.com>
2025-09-07 17:34:55 +0800

    add .gitignore

--- pkg/gormer.go
+++ pkg/gormer.go
@@ -627,15 +627,3 @@ func QueryAll[T any](tdc *gorm.DB, qc *QueryListConfig[T]) ([]T, error) {
 	return qr.Data, nil
 
 }
-
-func CreateOrUpdate[T any](tdc *gorm.DB, t *T, query *QueryConfig[T]) error {
-	exist, err := Exist[T](tdc, query)
-	if err != nil {
-		return err
-	}
-	if exist {
-		return Update[T](tdc, t)
-	} else {
-		return Create[T](tdc, *t)
-	}
-}
